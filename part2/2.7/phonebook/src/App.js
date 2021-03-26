1b22ac64ad68d7b163b907e0aad8f4347a111d68
Joseph Marie Alba <josephalbaph@gmail.com>
2021-03-27 07:17:35 +0800

    Add files via upload

---
+++ part2/2.7/phonebook/src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+
+    if (persons.find( p => p.name === newName )) {
+      window.alert(`${newName} is already added to phonebook`);
+    } else {
+      const personObject = {
+        name: newName,
+        id: persons.length + 1,
+      };
+      setPersons(persons.concat(personObject));
+      setNewName("");
+    }
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => (
+          <li>{person.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
