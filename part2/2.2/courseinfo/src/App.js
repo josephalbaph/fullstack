f061e52e7be61788edf431a04c5f53b2005ea8dc
Joseph Marie Alba <josephalbaph@gmail.com>
2021-03-26 23:21:32 +0800

    Add files via upload

---
+++ part2/2.2/courseinfo/src/App.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+const Header = ({ name }) => <h1>{name}</h1>;
+
+const Part = ({ name, exercises }) => (
+  <p>
+    {name} {exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map((part) => (
+      <Part key={part.id} name={part.name} exercises={part.exercises} />
+    ))}
+  </div>
+);
+
+const Total = ({ parts }) => (
+  <div>
+    <strong>
+      total of {parts.reduce((total, part) => (total += part.exercises), 0)} 
+      {" "} exercises
+    </strong>
+  </div>
+);
+
+const Course = ({ course }) => (
+  <div>
+    <Header name={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
+);
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
