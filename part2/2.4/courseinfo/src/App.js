6a15c76214d540718868d810391c24999f44a731
Joseph Marie Alba <josephalbaph@gmail.com>
2021-03-27 00:30:05 +0800

    Add files via upload

---
+++ part2/2.4/courseinfo/src/App.js
@@ -0,0 +1,93 @@
+import React from "react";
+
+const Header = ({ name }) => <h2>{name}</h2>;
+
+const Part = ({ name, exercises }) => (
+  <p>
+    {name} {exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map((part) => (
+      <Part key={part.id} name={part.name} exercises={part.exercises} />
+    ))}
+  </div>
+);
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((s, p) => (s += p.exercises), 0);
+  return (
+    <div>
+      <strong>total of {total} exercises</strong>
+    </div>
+  );
+};
+
+const Course = ({ course }) => (
+  <div>
+    <Header name={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
+);
+
+const Courses = ({ courses }) => (
+  <div>
+    <h1>Web development curriculum</h1>
+    {courses.map((course) => (
+      <Course key={course.id} course={course} />
+    ))}
+  </div>
+);
+
+const App = () => {
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
+  return <Courses courses={courses} />;
+};
+
+export default App;
