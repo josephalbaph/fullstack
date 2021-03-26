0d0e448827c0bf23a0c88a649722ff059db804fd
Joseph Marie Alba <josephalbaph@gmail.com>
2021-03-26 15:22:42 +0800

    Add files via upload

---
+++ part1/1.13/anecdotes/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+  ];
+  const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState(Array(anecdotes.length).fill(0))
+  const randomQuote = () => Math.floor(anecdotes.length * Math.random());
+  const updatePoints = () => {
+    const copyPoints = [...points]
+    copyPoints[selected]++
+    return copyPoints
+  }
+  return (
+    <div>
+      <div>{anecdotes[selected]}</div>
+      <div>has {points[selected]} votes</div>
+      <button onClick={() => setPoints(updatePoints())}>
+        vote
+      </button>
+      <button onClick={() => setSelected(randomQuote())}>
+        next anecdote
+      </button>
+    </div>
+  );
+};
+
+export default App;
