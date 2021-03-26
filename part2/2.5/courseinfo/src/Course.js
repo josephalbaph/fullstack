5ec4d1a1628c74733b4b6492cdf81b7dec0cfde6
Joseph Marie Alba <josephalbaph@gmail.com>
2021-03-27 06:14:07 +0800

    Add files via upload

---
+++ part2/2.5/courseinfo/src/Course.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Header = ({ name }) => <h2>{name}</h2>;
+
+const Part = ({ name, exercises }) => (
+  <p>
+    {name} {exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map((part) => (
+      <Part key={part.id} name={part.name} exercises={part.exercises} />
+    ))}
+  </div>
+);
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((s, p) => (s += p.exercises), 0);
+  return (
+    <div>
+      <strong>total of {total} exercises</strong>
+    </div>
+  );
+};
+
+const Course = ({ course }) => (
+  <div>
+    <Header name={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
+);
+
+
+export default Course;
