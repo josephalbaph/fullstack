8b98da97e0205bcafebdfbbd6d0773cf138f02ec
Joseph Marie Alba <josephalbaph@gmail.com>
2021-03-26 08:40:44 +0800

    Add files via upload

---
+++ part1/1.7/unicafe/src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+
+const Header = ({text}) => <h1>{text}</h1>
+
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const Display = ({text, score}) => (
+  <p> {text} {score}</p>
+)
+
+const DisplayPercent = ({text, score}) => (
+  <p> {text} {score*100} %</p>
+)
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const score_all = () => good + neutral + bad
+  const score_average = () => score_all() ? (good - bad) / score_all() : 0
+  const score_positive = () => score_all() ? good / score_all() : 0
+
+  return (
+    <div>
+      <Header text="give feedback"/>
+      <Button handleClick={()=>setGood(good+1)} text="good" />
+      <Button handleClick={()=>setNeutral(neutral+1)} text="neutral" />
+      <Button handleClick={()=>setBad(bad+1)} text="bad" />
+      <Header text="statistics"/>
+      <Display text="good" score={good} />
+      <Display text="neutral" score={neutral} />
+      <Display text="bad" score={bad} />
+      <Display text="all" score={score_all()} />
+      <Display text="average" score={score_average()} />
+      <DisplayPercent text="positive" score={score_positive()} />
+    </div>
+  )
+}
+
+export default App
